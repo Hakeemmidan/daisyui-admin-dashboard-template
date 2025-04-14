388b3bf69fc95313d44be4c82a810863c5649e95
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2025-04-14 22:00:39 +0300

    Put back the notification when a date changes in the analytics page

--- src/features/charts/index.js
+++ src/features/charts/index.js
@@ -1,58 +1,59 @@
-import LineChart from './components/LineChart'
-import BarChart from './components/BarChart'
-import DoughnutChart from './components/DoughnutChart'
-import PieChart from './components/PieChart'
-import ScatterChart from './components/ScatterChart'
-import StackBarChart from './components/StackBarChart'
-import Datepicker from "react-tailwindcss-datepicker"; 
-import { useState } from 'react'
+import LineChart from "./components/LineChart";
+import BarChart from "./components/BarChart";
+import DoughnutChart from "./components/DoughnutChart";
+import PieChart from "./components/PieChart";
+import ScatterChart from "./components/ScatterChart";
+import StackBarChart from "./components/StackBarChart";
+import Datepicker from "react-tailwindcss-datepicker";
+import { useState } from "react";
+import useStore from "../../app/store_zustand";
 
+function Charts() {
+  const showNotification = useStore((state) => state.showNotification);
 
+  const [dateValue, setDateValue] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
 
+  const handleDatePickerValueChange = (newValue) => {
+    setDateValue(newValue);
+    showNotification({
+      message: `Date range updated to ${newValue.startDate} - ${newValue.endDate}`,
+      status: 1,
+    });
+  };
 
-function Charts(){
+  return (
+    <>
+      <Datepicker
+        containerClassName="w-72"
+        value={dateValue}
+        theme={"light"}
+        inputClassName="input input-bordered w-72"
+        popoverDirection={"down"}
+        toggleClassName="invisible"
+        onChange={handleDatePickerValueChange}
+        showShortcuts={true}
+        primaryColor={"white"}
+      />
+      {/** ---------------------- Different charts ------------------------- */}
+      <div className="grid lg:grid-cols-2 mt-0 grid-cols-1 gap-6">
+        <StackBarChart />
+        <BarChart />
+      </div>
 
-    const [dateValue, setDateValue] = useState({ 
-        startDate: new Date(), 
-        endDate: new Date() 
-    }); 
-    
-    const handleDatePickerValueChange = (newValue) => {
-        console.log("newValue:", newValue); 
-        setDateValue(newValue); 
-    } 
+      <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
+        <DoughnutChart />
+        <PieChart />
+      </div>
 
-    return(
-        <>
-        <Datepicker 
-                containerClassName="w-72" 
-                value={dateValue} 
-                theme={"light"}
-                inputClassName="input input-bordered w-72" 
-                popoverDirection={"down"}
-                toggleClassName="invisible"
-                onChange={handleDatePickerValueChange} 
-                showShortcuts={true} 
-                primaryColor={"white"} 
-            /> 
-        {/** ---------------------- Different charts ------------------------- */}
-            <div className="grid lg:grid-cols-2 mt-0 grid-cols-1 gap-6">
-                <StackBarChart />
-                <BarChart />
-            </div>
-
-        
-            <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
-                <DoughnutChart />
-                <PieChart />
-            </div>
-
-            <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
-                <ScatterChart />
-                <LineChart />
-            </div>
-        </>
-    )
+      <div className="grid lg:grid-cols-2 mt-4 grid-cols-1 gap-6">
+        <ScatterChart />
+        <LineChart />
+      </div>
+    </>
+  );
 }
 
-export default Charts
+export default Charts;
